cd047b53c05c5512cce8b0fc2e7a709a966248e8
Divya Gaurav <divyag.ug16.ee@nitp.ac.in>
2022-11-22 02:25:35 +0530

    giving style to meal Item and adding Form to every item

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import classes from './MealItem.module.css'
+import MealItemForm from './MealItemForm'
 
 const MealItem = (props) => {
     const price = `₹${props.price.toFixed(2)}`
     return (
         <li className={classes.meal}>
-            <div><h3>{props.name}</h3></div>
-            <div className={classes.description}>{props.description}</div>
-            <div className={classes.price}>{price}</div>
-            <div> </div>
             <div>
-
+                <h3>{props.name}</h3>
+                <div className={classes.description}>{props.description}</div>
+                <div className={classes.price}>{price}</div>
+            </div>
+            <div>
+              <MealItemForm/>
             </div>
         </li>
     )
